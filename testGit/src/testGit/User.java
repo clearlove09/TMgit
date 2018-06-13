84c630c15ff10bc31bb58680f1eeaaba3f871f72
clearlove09 <faithcjs@163.com>
2018-06-13 19:06:19 +0800

    修改user.java

--- testGit/src/testGit/User.java
+++ testGit/src/testGit/User.java
@@ -4,5 +4,30 @@ public class User {
 	
 	private Integer userAge;
 	private String name;
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public User(Integer userAge, String name) {
+		super();
+		this.userAge = userAge;
+		this.name = name;
+	}
+	public Integer getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(Integer userAge) {
+		this.userAge = userAge;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "User [userAge=" + userAge + ", name=" + name + "]";
+	}
 
 }
